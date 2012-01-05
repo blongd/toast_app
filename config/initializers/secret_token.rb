37bcf63701c8ec892def435b0d5bf01a7906e137
Devin Blong <devin@kidbombay.com>
2012-01-04 19:13:37 -0800

    Initial commit. Toasting begin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ToastApp::Application.config.secret_token = '3d046d27be87abcef10fe2ea3d2928aee7584edc5463e2ef860e214f6d419a72b4524df83e2d6ffba3cb931fd02afbae52b9ff97ced478a5ea59860296a45a26'

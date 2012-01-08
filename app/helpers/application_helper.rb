726bc7a311dc051459603873d45c9e1bc465d48c
Devin Blong <devin@kidbombay.com>
2012-01-07 23:52:22 -0800

    Chap 5 Excersizes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,10 @@
 module ApplicationHelper
 
+  # Returns logo code
+  def logo
+     image_tag("toast_pic.jpg", :alt => "Toast Machine", :class => "round")
+   end
+  
   # Return a title on a per-page basis.
   def title
     base_title = "This app is a Toast Machine"
